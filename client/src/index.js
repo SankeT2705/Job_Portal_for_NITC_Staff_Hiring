5199d9584a85dfca6a39f94ce2d59436fbe3381b
SankeT2705 <sanketofficial27@gmail.com>
2025-11-09 00:16:43 +0530

    removing google auth

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+ 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="358761858105-9rbetcc2nut9annem3c2ihd3un22lnrs.apps.googleusercontent.com">
-    <App />
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+  
+ 
 );
